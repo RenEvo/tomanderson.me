9af52d614d25b2c0ab801d3bba3863fbc168bf46
Tom Anderson <dante.renevo@gmail.com>
2014-02-26 10:42:11 -0800

    Blog - Stateless State Machine Code

---
+++ Blog - Stateless State Machine/StatelessBlogPost/BasicStateMachine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatelessBlogPost
+{
+    public class BasicStateMachine
+    {
+        public string State { get; private set; }
+
+        public void TransitionToState(string state)
+        {
+            switch (state)
+            {
+                case "on":
+                    State = "ON";
+                    OnSwitchedOn();
+                    break;
+                default:
+                    State = "OFF";
+                    OnSwitchedOff();
+                    break;
+            }
+        }
+
+        private void OnSwitchedOn()
+        {
+            // Do something clever
+        }
+
+        private void OnSwitchedOff()
+        {
+            // Stop doing something clever
+        }
+    }
+}
---
+++ Blog - Stateless State Machine/StatelessBlogPost/StatelessStateMachine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace StatelessBlogPost
+{
+    public class StatelessStateMachine
+    {
+        private readonly StateMachine<State, Trigger> _stateMachine;
+
+        public enum Trigger
+        {
+            TurnOn,
+            TurnOff
+        }
+
+        public enum State
+        {
+            On,
+            Off
+        }
+
+        public State Current { get { return _stateMachine.State; } }
+
+        public StatelessStateMachine()
+        {
+            _stateMachine = new StateMachine<State, Trigger>(State.Off);
+            _stateMachine.Configure(State.Off)
+                .Permit(Trigger.TurnOn, State.On)
+                .OnEntry(OnSwitchedOn);
+
+            _stateMachine.Configure(State.On)
+                .Permit(Trigger.TurnOff, State.Off)
+                .OnEntry(OnSwitchedOff);
+        }
+
+        public bool Transition(Trigger trigger)
+        {
+            if (!_stateMachine.CanFire(trigger))
+                return false;
+
+            _stateMachine.Fire(trigger);
+
+            return true;
+        }
+        private void OnSwitchedOn()
+        {
+            // Do something clever
+        }
+
+        private void OnSwitchedOff()
+        {
+            // Stop doing something clever
+        }
+
+    }
+}
